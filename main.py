65defde9e78462025ab9a013632520ef80874324
lucimar1985 <114201220+lucimar1985@users.noreply.github.com>
2024-01-18 13:00:32 -0300

    commit atualização

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# main file.
+import openai
+import pyttsx3
+import speech_recognition as sr
+
+# set openai api key
+openai.api_key - "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"
+
+# .initialize the text-to-speech engine
+engine - pyttsx3.init()
+
+
+# set the voice to a portuguese voice
+voices - engine.getProperty
